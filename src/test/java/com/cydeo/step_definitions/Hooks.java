fe876fe65550f83393349fe1201623890baa8a7f
Jasmine <bayramali5219@gmail.com>
2022-06-20 15:32:53 +0300

    screenshot practice

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -3,6 +3,7 @@ package com.cydeo.step_definitions;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.sl.In;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -30,11 +31,21 @@ public class Hooks {
 
 
    @After
-     public void teardownScenario(){
+     public void teardownScenario(Scenario scenario){
+
+        //scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
 
 
-            byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()) {
 
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+
+
+
+
+        //BrowserUtils.sleep(5);
         Driver.closeDriver();
 
         //System.out.println("====Closing browser using cucumber @After");
